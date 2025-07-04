23790f70c49dd2ff84c65886618c8290b50d4b4d
Balramog <balrampreet116699@gmail.com>
2025-07-04 19:30:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# Mount GDrive to /music
+
+# Mount Google Drive using rclone
+echo "Mounting Google Drive..."
 rclone mount gdrive:/Navidrome /music --daemon
 
-# Start Navidrome
+# Start Navidrome (reads from /music)
+echo "Starting Navidrome..."
 /app/navidrome
