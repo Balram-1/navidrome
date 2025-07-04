fa1ac7cd8b929a825cf7c0952c4d18ce2347d908
Balramog <balrampreet116699@gmail.com>
2025-07-04 19:40:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-chmod +x start.sh
-git add start.sh
-git commit -m "Make start.sh executable"
-git push
+#!/bin/sh
+
+# Sync music from GDrive to local
+rclone copy gdrive:/music /data/music --create-empty-src-dirs --drive-acknowledge-abuse=true --transfers=4 --checkers=8 --drive-chunk-size=64M
+
+# Start navidrome
+/navidrome
